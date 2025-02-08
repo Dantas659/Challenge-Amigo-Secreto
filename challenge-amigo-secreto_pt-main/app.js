d6e72bd3d0355ff458472f06354041b92cd618c3
Lívia Dantas <liviadantas659@gmail.com>
2025-02-08 17:22:12 -0300

    Função para adicionar amigos e suas respectivas funcionalidades criada.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomesDosAmigos= [];
+
+function adicionarAmigo() {
+    var nomesFornecidos = document.querySelector('input').value
+    if(nomesFornecidos !== "") {
+        nomesDosAmigos.push(nomesFornecidos);
+    } else{
+        validarEntrada();
+    }
+    exibirListaDeAmigos('listaAmigos', nomesDosAmigos);
+    limparCampo();
+}
+
+function limparCampo() {
+    nomesFornecidos = document.querySelector('input');
+    nomesFornecidos.value = '';
+}
+
+function validarEntrada() {
+    let campo = document.querySelector('input').value;
+    if(campo === "") {
+        alert('O campo não pode estar vazio!');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function exibirListaDeAmigos(id, nomesDosAmigos) {
+    let campo = document.getElementById(id);
+    campo.innerHTML = nomesDosAmigos;
+}  
